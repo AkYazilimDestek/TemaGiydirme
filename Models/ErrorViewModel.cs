f17ae1cb9755695cb2ffe0b4098b5a8371f3ea3c
Ahmet Akın <akinahmed277@gmail.com>
2023-10-04 20:58:57 +0300

    proje

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebTemaGiydirme.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
